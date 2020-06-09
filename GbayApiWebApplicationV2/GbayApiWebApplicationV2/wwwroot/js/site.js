40ebc684206217d60cba590802750af88e6007ee
Grant <grantshanklin@gmail.com>
2020-06-09 09:13:03 -0500

    Created login modal

--- GbayApiWebApplicationV2/GbayApiWebApplicationV2/wwwroot/js/site.js
+++ GbayApiWebApplicationV2/GbayApiWebApplicationV2/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById("loginsubmit").addEventListener("click", e => {
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    data = { 'username': username, 'email': email };
+    myFetch('/api/v1/Login', 'POST', false, data)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+                //return response.json();
+            }
+        })
+        .then(data => {
+            //sessionStorage.setItem('jwt', data.token);            
+            //document.location.href = '/';
+            //isLoggedIn();
+            //$('#modalLogin').modal('hide');
+            document.getElementById('loginButton').classList.add('d-none');
+            document.getElementById('confirmTwoFactorButton').classList.remove('d-none');
+            document.getElementById('loginUsernamePasswordContainer').classList.add('d-none');
+            document.getElementById('loginTwoFactorFormGroup').classList.remove('d-none');
+            enableLoginSpinner(false);
+        })
+        .catch(error => {
+            console.log(error);
+            document.getElementById('loginStatus').innerText = "Login Failed";
+            enableLoginSpinner(false);
+        });
+});
