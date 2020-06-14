5f9b55180e77d56e631b60f8a09772217659ede6
Grant <grantshanklin@gmail.com>
2020-06-14 17:11:33 -0500

    Added cart basics

--- GbayApiWebApplicationV2/GbayApiWebApplicationV2/ApiControllers/AuthorizeCheckController.cs
+++ GbayApiWebApplicationV2/GbayApiWebApplicationV2/ApiControllers/AuthorizeCheckController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GbayApiWebApplicationV2.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using GbayApiWebApplicationV2.ViewModels;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,12 +15,31 @@ namespace GbayApiWebApplicationV2.ApiControllers
     [Route("api/[controller]")]
     public class AuthorizeCheckController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AuthorizeCheckController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         // GET: api/<controller>
         [HttpGet]
         [Authorize]
-        public ActionResult Get()
+        public async Task<ActionResult> Get()
         {
-            return new OkResult();
+            ApplicationUser user = await userManager.FindByIdAsync(User.Identity.Name);
+            if (user != null)
+            {
+                AuthorizeCheckViewModel model = new AuthorizeCheckViewModel
+                {
+                    Username = user.UserName
+                };
+                return new OkObjectResult(model);
+            }
+            else
+            {
+                return new OkResult();
+            }
         }
     }
 }
