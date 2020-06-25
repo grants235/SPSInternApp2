3e62137c1092c947bee79bac3495e27cb65cfb6b
Grant <grantshanklin@gmail.com>
2020-06-25 15:50:05 -0500

    Improved edit product functionality

--- GbayApiWebApplicationV2/GbayApiWebApplicationV2/wwwroot/js/EditProduct.js
+++ GbayApiWebApplicationV2/GbayApiWebApplicationV2/wwwroot/js/EditProduct.js
@@ -22,6 +22,102 @@
     }
 });
 
+document.body.addEventListener('click', e => {
+    if (e.srcElement.id == 'EditProductModal1') {
+        var jwt = sessionStorage.getItem('jwt');
+        if (jwt != null && jwt != " ") {
+            var requestInfo = { 'method': 'GET', headers: { 'Authorization': 'bearer ' + jwt }, credentials: 'same-origin' };
+            fetch('/api/ManageProducts', requestInfo)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    document.getElementById('EditProductName').value = data.products[1].productName;
+                    document.getElementById('EditProductDescription').value = data.products[1].description;
+                    document.getElementById('EditProductPrice').value = data.products[1].price;
+                    document.getElementById('EditProductImageUrl').value = data.products[1].imgUrl;
+                    document.getElementById('EditProductId').value = data.products[1].id;
+
+                    $("#EditProductModal").modal('show');
+                });
+        }
+    }
+});
+
+document.body.addEventListener('click', e => {
+    if (e.srcElement.id == 'EditProductModal2') {
+        var jwt = sessionStorage.getItem('jwt');
+        if (jwt != null && jwt != " ") {
+            var requestInfo = { 'method': 'GET', headers: { 'Authorization': 'bearer ' + jwt }, credentials: 'same-origin' };
+            fetch('/api/ManageProducts', requestInfo)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    document.getElementById('EditProductName').value = data.products[2].productName;
+                    document.getElementById('EditProductDescription').value = data.products[2].description;
+                    document.getElementById('EditProductPrice').value = data.products[2].price;
+                    document.getElementById('EditProductImageUrl').value = data.products[2].imgUrl;
+                    document.getElementById('EditProductId').value = data.products[2].id;
+
+                    $("#EditProductModal").modal('show');
+                });
+        }
+    }
+});
+
+document.body.addEventListener('click', e => {
+    if (e.srcElement.id == 'EditProductModal3') {
+        var jwt = sessionStorage.getItem('jwt');
+        if (jwt != null && jwt != " ") {
+            var requestInfo = { 'method': 'GET', headers: { 'Authorization': 'bearer ' + jwt }, credentials: 'same-origin' };
+            fetch('/api/ManageProducts', requestInfo)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    document.getElementById('EditProductName').value = data.products[3].productName;
+                    document.getElementById('EditProductDescription').value = data.products[3].description;
+                    document.getElementById('EditProductPrice').value = data.products[3].price;
+                    document.getElementById('EditProductImageUrl').value = data.products[3].imgUrl;
+                    document.getElementById('EditProductId').value = data.products[3].id;
+
+                    $("#EditProductModal").modal('show');
+                });
+        }
+    }
+});
+
+document.body.addEventListener('click', e => {
+    if (e.srcElement.id == 'EditProductModal4') {
+        var jwt = sessionStorage.getItem('jwt');
+        if (jwt != null && jwt != " ") {
+            var requestInfo = { 'method': 'GET', headers: { 'Authorization': 'bearer ' + jwt }, credentials: 'same-origin' };
+            fetch('/api/ManageProducts', requestInfo)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                    document.getElementById('EditProductName').value = data.products[4].productName;
+                    document.getElementById('EditProductDescription').value = data.products[4].description;
+                    document.getElementById('EditProductPrice').value = data.products[4].price;
+                    document.getElementById('EditProductImageUrl').value = data.products[4].imgUrl;
+                    document.getElementById('EditProductId').value = data.products[4].id;
+
+                    $("#EditProductModal").modal('show');
+                });
+        }
+    }
+});
+
 document.getElementById('EditProductSubmit').addEventListener('click', e => {
     var jwt = sessionStorage.getItem('jwt');
     if (jwt != null && jwt != " ") {
